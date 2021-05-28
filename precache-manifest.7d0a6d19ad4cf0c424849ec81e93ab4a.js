e03d6458ca7c4fca62187df29717fcbba490658d
Yulin Zhang <yzhan114@ncsu.edu>
2021-05-27 22:34:56 -0400

    Updates

---
+++ precache-manifest.7d0a6d19ad4cf0c424849ec81e93ab4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb7824e13712061e2ca725860b96a22",
+    "url": "/zoom-studies-v3/index.html"
+  },
+  {
+    "revision": "71ef614cff88a9c0454e",
+    "url": "/zoom-studies-v3/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "ff410b2a79bc86d52f2c",
+    "url": "/zoom-studies-v3/static/css/main.d8ba6ee7.chunk.css"
+  },
+  {
+    "revision": "71ef614cff88a9c0454e",
+    "url": "/zoom-studies-v3/static/js/2.5d382ba3.chunk.js"
+  },
+  {
+    "revision": "a458991a84e8e095474d6165ab54d251",
+    "url": "/zoom-studies-v3/static/js/2.5d382ba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff410b2a79bc86d52f2c",
+    "url": "/zoom-studies-v3/static/js/main.9cb0db76.chunk.js"
+  },
+  {
+    "revision": "342e0523d2bf1c8c56df",
+    "url": "/zoom-studies-v3/static/js/runtime-main.106b6b8f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/zoom-studies-v3/precache-manifest.7d0a6d19ad4cf0c424849ec81e93ab4a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zoom-studies-v3/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
